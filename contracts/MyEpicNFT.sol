f1da82e8030de601fa68a78c7e80b32b304bb210
Preston <prestonp2018@gmail.com>
2022-04-26 18:25:23 -0400

    start create contract that mints NFTs

---
+++ contracts/MyEpicNFT.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+import "hardhat/console.sol";
+
+pragma solidity ^0.8.1;
+
+import "hardhat/console.sol";
+
+contract MyEpicNFT {
+    constructor() {
+        console.log("This is my NFT contract. Whoa!");
+    }
+}
